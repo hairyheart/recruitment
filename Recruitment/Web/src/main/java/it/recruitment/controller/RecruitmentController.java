a59ce074f2f5767e0e4e8aefc96247ab03a5b836
unknown <genny.sgm14@gmail.com>
2019-07-16 23:09:10 +0200

    Log4J and Controller annotation added - Log4j.xml configurated

--- Recruitment/Web/src/main/java/it/recruitment/controller/RecruitmentController.java
+++ Recruitment/Web/src/main/java/it/recruitment/controller/RecruitmentController.java
@@ -1,5 +1,12 @@
 package it.recruitment.controller;
 
+import org.springframework.stereotype.Controller;
+
+import org.apache.log4j.Logger;
+
+@Controller
 public class RecruitmentController {
+
+	private static final Logger logger = Logger.getLogger(RecruitmentController.class);
 
 }
